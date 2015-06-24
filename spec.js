e998fd623abb585e00d985c38ad76dce0af9d0d9
brittanya <m.brittanya@gmail.com>
2015-06-24 17:44:16 -0600

    adding testing files

---
+++ spec.js
@@ -0,0 +1,29 @@
+describe('basic calcular function list', function() {
+  
+  it('should add numbers', function() {
+    browser.get('http://mbrittanya.github.io/grid-demo')
+
+    element(by.id('but1')).click();
+    element(by.id('butAdd')).click();
+    element(by.id('but2')).click();
+    element(by.id('butEq')).click();
+
+    expect(element(by.binding('total')).getText()).
+        toEqual('3');
+  });
+
+  it('should subtract numbers', function() {
+    //leave this blank for now
+  });
+
+  it('should multiply numbers', function() {
+    //leave this blank for now
+  });
+
+  it('should divide numbers', function() {
+    //leave this blank for now
+  });
+  it('should handle numbers with decimals', function() {
+    //leave this blank for now
+  });
+});
